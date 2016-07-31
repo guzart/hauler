4d7d70c8885f0bfd34c7275869f8276a82f1eef6
Arturo Guzman <arturo@guzart.com>
2016-07-31 01:17:04 -0400

    fix(npm): fix building package

--- make-package.js
+++ make-package.js
@@ -1,8 +1,9 @@
 #!/usr/bin/env node --use_strict
 
 const fs = require('fs');
+const path = require('path');
 
-const pkg = require('./package.json');
+const pkg = Object.assign({}, require('./package.json'));
 delete pkg.scripts;
 Object.assign(pkg, {
   main: 'index.js',
@@ -11,3 +12,12 @@ Object.assign(pkg, {
 fs.writeFileSync('dist/package.json', JSON.stringify(pkg, null, 2));
 fs.writeFileSync('dist/LICENSE.txt', fs.readFileSync('./LICENSE.txt').toString());
 fs.writeFileSync('dist/README.md', fs.readFileSync('./README.md').toString());
+
+fs.mkdirSync('dist/bin');
+Object.keys(pkg.bin).forEach(key => {
+  const binaryPath = pkg.bin[key];
+  fs.writeFileSync(
+    path.join('dist', binaryPath),
+    fs.readFileSync(binaryPath).toString()
+  );
+});
