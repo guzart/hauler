b0be22d37b155d4f31ff5c2267b8d332d0c4ef9a
Arturo Guzman <arturo@guzart.com>
2016-07-31 00:16:01 -0400

    feat(install): add linter and webpack configuration files

--- lib/generators/spiral/install_generator.rb
+++ lib/generators/spiral/install_generator.rb
@@ -2,16 +2,28 @@ module Spiral
   module Generators
     #:nodoc:
     class InstallGenerator < ::Rails::Generators::Base
-      desc 'Copy Spiral default files'
+      desc 'Copy Spiral configuration files'
       source_root File.expand_path('../templates', __FILE__)
 
-      def copy_config
+      def copy_config_spiral
         template 'config/spiral.js'
       end
 
-      def copy_npm_package_json
+      def copy_eslintrc
+        template 'eslintrc.json', '.eslintrc.json'
+      end
+
+      def copy_sass_lint_yml
+        template 'sass-lint.yml', '.sass-lint.yml'
+      end
+
+      def copy_package_json
         has_package_json = File.exist?(Rails.root.join('package.json'))
         template 'package.json' if !has_package_json
+      end
+
+      def copy_webpack_config
+        template 'webpack.config.js'
       end
 
       # def add_npm_start_script
