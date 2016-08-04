17740bc7b32f2a2fe009c8d0925c603a60a8cdd7
Arturo Guzman <arturo@kisurfer.com>
2016-08-04 00:37:07 -0400

    chore(tests): add jest and wallaby

---
+++ wallaby.js
@@ -0,0 +1,20 @@
+module.exports = function (wallaby) {
+  return {
+    files: ['src/**/*.js', '!src/**/__tests__/*.js'],
+
+    tests: ['src/**/__tests__/*.js'],
+
+    compilers: {
+      'src/**/*.js': wallaby.compilers.babel()
+    },
+
+    env: {
+      type: 'node',
+      runner: 'node'
+    },
+
+    testFramework: 'jest',
+
+    debug: true,
+  };
+};
