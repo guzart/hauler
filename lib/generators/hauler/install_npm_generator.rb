b07a94cfbc6dcd1359334b8c782086cec67dbaad
Arturo Guzman <arturo@kisurfer.com>
2016-08-14 01:40:20 -0400

    chore(generator): change order in which npm packages get installed
    Prevents showing the unmet dependencies error.

--- lib/generators/hauler/install_npm_generator.rb
+++ lib/generators/hauler/install_npm_generator.rb
@@ -5,6 +5,30 @@ module Hauler
       desc 'Setup hauler project'
       source_root File.expand_path('../templates', __FILE__)
 
+      NPM_DEPENDENCIES = <<-TXT.freeze
+        babel-polyfill@6.9.1
+        whatwg-fetch@1.0.0
+      TXT
+
+      REACT_NPM_DEPENDENCIES = <<-TXT.freeze
+        immutable@3.8.1
+        react@15.2.1
+        react-dom@15.2.1
+        react-redux@4.4.5
+        react-router@2.6.1
+        react-router-redux@4.0.5
+        react-router-scroll@0.2.1
+        redux@3.5.2
+        redux-immutable@3.0.6
+        redux-saga@0.11.0
+        reselect@2.5.3
+      TXT
+
+      SASS_NPM_DEV_DEPENDENCIES = <<-TXT.freeze
+        node-sass@3.8.0
+        sass-loader@4.0.0
+      TXT
+
       NPM_DEV_DEPENDENCIES = <<-TXT.freeze
         babel-core@6.11.4
         babel-loader@6.2.4
@@ -44,38 +68,9 @@ module Hauler
         redux-logger@2.6.1
       TXT
 
-      REACT_NPM_DEPENDENCIES = <<-TXT.freeze
-        immutable@3.8.1
-        react@15.2.1
-        react-dom@15.2.1
-        react-redux@4.4.5
-        react-router@2.6.1
-        react-router-redux@4.0.5
-        react-router-scroll@0.2.1
-        redux@3.5.2
-        redux-immutable@3.0.6
-        redux-saga@0.11.0
-        reselect@2.5.3
-      TXT
-
-      SASS_NPM_DEV_DEPENDENCIES = <<-TXT.freeze
-        node-sass@3.8.0
-        sass-loader@4.0.0
-      TXT
-
-      NPM_DEPENDENCIES = <<-TXT.freeze
-        babel-polyfill@6.9.1
-        whatwg-fetch@1.0.0
-      TXT
-
-      def install_dev_dependencies
-        puts 'Installing NPM dev dependencies...'
-        install_deps(NPM_DEV_DEPENDENCIES)
-      end
-
-      def install_linter_dev_dependencies
-        puts 'Installing Linter NPM dev dependencies...'
-        install_deps(LINTER_DEV_DEPENDENCES)
+      def install_dependencies
+        puts 'Installing NPM dependencies...'
+        install_deps(NPM_DEPENDENCIES, dev: false)
       end
 
       def install_react_dependencies
@@ -91,9 +86,14 @@ module Hauler
         install_deps(SASS_NPM_DEV_DEPENDENCIES)
       end
 
-      def install_dependencies
-        puts 'Installing NPM dependencies...'
-        install_deps(NPM_DEPENDENCIES, dev: false)
+      def install_dev_dependencies
+        puts 'Installing NPM dev dependencies...'
+        install_deps(NPM_DEV_DEPENDENCIES)
+      end
+
+      def install_linter_dev_dependencies
+        puts 'Installing Linter NPM dev dependencies...'
+        install_deps(LINTER_DEV_DEPENDENCES)
       end
 
       def add_scripts_to_package
