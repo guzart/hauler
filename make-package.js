b5ee775adcc4276771cfeaa55cfc20346b256d48
Arturo Guzman <arturo@guzart.com>
2016-08-16 23:35:07 -0400

    chore(npm): move npm package binaries to src subfolder

--- make-package.js
+++ make-package.js
@@ -1,23 +1,20 @@
 #!/usr/bin/env node --use_strict
 
 const fs = require('fs');
-const path = require('path');
+
+fs.mkdirSync('dist');
 
 const pkg = Object.assign({}, require('./package.json'));
 delete pkg.scripts;
 Object.assign(pkg, {
   main: 'index.js',
+  bin: {
+    'hauler-read-config': 'bin/read-config.js',
+    'hauler-server': 'bin/dev-server.js',
+    'hauler-update-scripts': 'bin/update-scripts.js'
+  }
 });
 
 fs.writeFileSync('dist/package.json', JSON.stringify(pkg, null, 2));
 fs.writeFileSync('dist/LICENSE.txt', fs.readFileSync('./LICENSE.txt').toString());
 fs.writeFileSync('dist/README.md', fs.readFileSync('./README.md').toString());
-
-fs.mkdirSync('dist/bin');
-Object.keys(pkg.bin).forEach(key => {
-  const binaryPath = pkg.bin[key];
-  fs.writeFileSync(
-    path.join('dist', binaryPath),
-    fs.readFileSync(binaryPath).toString()
-  );
-});
